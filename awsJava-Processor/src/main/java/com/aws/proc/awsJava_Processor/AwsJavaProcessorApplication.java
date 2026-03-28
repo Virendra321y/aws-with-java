d89404ed9a78ae8a35d840742a84aacda65803b7
Virendra321y <virendrayadavvns123@gmail.com>
2026-03-28 22:16:01 +0530

    added processor microservice

---
+++ awsJava-Processor/src/main/java/com/aws/proc/awsJava_Processor/AwsJavaProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.aws.proc.awsJava_Processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJavaProcessorApplication.class, args);
+	}
+
+}
---
+++ awsJava-Processor/src/test/java/com/aws/proc/awsJava_Processor/AwsJavaProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.proc.awsJava_Processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJavaProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
