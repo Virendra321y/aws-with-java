cf7c9a66a8bf72395729de862c5c163b42b47454
Virendra321y <virendrayadavvns123@gmail.com>
2026-03-28 22:08:41 +0530

    added gateway service

---
+++ awsJava-Gateway/src/main/java/com/aws/vir/awsJava_Gateway/AwsJavaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aws.vir.awsJava_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJavaGatewayApplication.class, args);
+	}
+
+}
---
+++ awsJava-Gateway/src/test/java/com/aws/vir/awsJava_Gateway/AwsJavaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.vir.awsJava_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJavaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
